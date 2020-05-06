5465302e198cec547fc7af0b44b2773e2e03f07f
Aseina <kotaksuratsulis@gmail.com>
2020-05-06 16:13:37 +0700

    praktek javascript dasar

---
+++ app.js
@@ -0,0 +1,9 @@
+const age = 16;
+
+if (age > 18) {
+  console.log("you are good to go");
+} else if (age < 15) {
+  console.log("wow kamu sangat muda, kenapa disini?");
+} else {
+  console.log("umur kamu tidak cukup!");
+}
