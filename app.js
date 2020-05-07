2e1fcfc7a2cd43146ffea7774cb9037a78f49a72
Aseina <kotaksuratsulis@gmail.com>
2020-05-07 07:03:35 +0700

    belajar javascript basic sampai array

--- app.js
+++ app.js
@@ -1,2 +1,40 @@
-var name = "Daniel";
-console.log(name);
+/*
+=
++=
+-=
+/=
+%=
+*/
+
+var number = 10;
+//number = number +5;
+number += 5;
+
+//Arithmetic Operators
+/*
+=
+-
+*
+/
+*/
+
+var number2 = 5 * 10;
+
+console.log(number);
+console.log(number2);
+
+//String Operators
+var age = 27;
+var info = "My name is Daniel, I am " + age + " years old" + 10;
+console.log(info);
+
+var numbers = 4 + " " + 5;
+console.log(numbers);
+
+if (age == 27 && number2 == 40) {
+  console.log("If!");
+} else if ((number = 10)) {
+  console.log("else if");
+} else {
+  console.log("Else!");
+}
